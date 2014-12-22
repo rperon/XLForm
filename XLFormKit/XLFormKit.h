99c1c0d6d04f7daf2ccabe31724758489b3697c7
Remi Peron <remi.peron@gmail.com>
2014-12-22 17:48:12 +0100

    Creating a Cocoa Touch Framework to use with carthage.

---
+++ XLFormKit/XLFormKit.h
@@ -0,0 +1,20 @@
+//
+//  XLFormKit.h
+//  XLFormKit
+//
+//  Created by Remi Peron on 22/12/2014.
+//  Copyright (c) 2014 Xmartlabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for XLFormKit.
+FOUNDATION_EXPORT double XLFormKitVersionNumber;
+
+//! Project version string for XLFormKit.
+FOUNDATION_EXPORT const unsigned char XLFormKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <XLFormKit/PublicHeader.h>
+
+
+#import <XLFormKit/XLForm.h>
